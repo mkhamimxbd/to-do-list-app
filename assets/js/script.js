6279c4d3aa2f03130c5871e3e7045a754c84c7a2
M. K. Hamim <mkhamimbd@gmail.com>
2026-01-01 10:22:45 +0600

    made the app interactive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,69 @@
-function addToDo() {
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+function renderToDo() {
   let todoHTML = '';
 
-  todoHTML = `
-    <p class="todo">Wash dishes</p>
-    <button class="todo-delete-btn">Delete</button>
-  `;
+  if (todoList) {
+    todoList.forEach((todo, index) => {
+      todoHTML += `
+          <p class="todo">${todo}</p>
+          <button class="todo-delete-btn js-todo-delete-btn" data-index="${index}">Delete</button>
+        `;
+    });
+  }
 
   document.querySelector('.js-todo-list-div')
-    .innerHTML += todoHTML;
+    .innerHTML = todoHTML;
 }
+
+function addToDo() {
+  const todoInput = document.querySelector('.js-todo-input');
+
+  if (todoInput.value) {
+    todoList.push(todoInput.value);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+
+    renderToDo();
+
+    todoInput.value = '';
+  }
+
+  activateDeleteButton();
+}
+
+function addToDoOnEnter() {
+  if (event.key === 'Enter') {
+    addToDo();
+  }
+}
+
+function activateDeleteButton() {
+  document.querySelectorAll('.js-todo-delete-btn').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const { index } = btn.dataset;
+      todoList.splice(index, 1);
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+      
+
+      renderToDo();
+      activateDeleteButton();
+    });
+  });
+}
+
+renderToDo();
+activateDeleteButton();
 
 document.querySelector('.js-todo-add-btn')
   .addEventListener('click', () => {
     addToDo();
   });
+
+document.querySelector('.js-todo-input')
+  .addEventListener('keydown', () => {
+    addToDoOnEnter();
+  });
+
+
+
+console.log(todoList);
